2573c9a705b6bf14c91bedd509d951b498ab798b
NinaNovachrono <100101615+NinaNovachrono@users.noreply.github.com>
2022-02-22 15:15:27 -0700

    Update peticiones.js

--- peticiones.js
+++ peticiones.js
@@ -1,84 +1,75 @@
-const cargarTipos = async()=>{
-    try{
-        const url = "http://201.140.116.237/services/tipo.php"
-        await axios 
+const cargarTipos=async()=>{
+    try {
+        const url ="http://201.140.116.237/services/tipo.php"
+        //const url ="tipos.json"
+        await axios
         .get(url)
         .then((res)=>{
-            llenarCombos(res.data);
+            llenarcombos(res.data)
+
         })
         .catch((err)=>{
-            console.log("Surgio un error en la peticiÃ³n" +err);
-            return false;
-        });
-    }catch(error){
-        console.log("Surgio un error"); 
-        return false;
-    }
-    return true;
+            console.log("Hubo un error en la petición "+err);
+            return false;})
+
+    } catch(error) { console.log("Surgio un error " +error); return false;
+      }
+      return true;
+
 }
 
-function llenarCombos(data){
-    for( let item of data){
-        document.getElementById("tipo").innerHTML += 
-        `<option value ="${item.tipo}">${item.descripcion}</option>`
+function llenarcombos(data){
+    for(let item of data){
+        document.getElementById('tipo').innerHTML += `
+        <option value ="${item.tipo}">${item.descripcion}</option>`
     }
-    document.getElementById('tipo').innerHTML+=`
-    </select>
-    `
+    document.getElementById('tipo').innerHTML += `</select>`
 };
 
-const cargarTablaVentas = async()=>{
-    try{
-        const urlVentas = "http://201.140.116.237/services/ventas.php" 
-        await axios 
-        .get(urlVentas)
+const cargarVentas=async()=>{
+    try {
+        const url = "http://201.140.116.237/services/ventas.php";
+        //url ="ventas.json";
+        await axios
+        .get(url)
         .then((res)=>{
             dibujarTabla(res.data);
-    })
-    .catch((err)=>{
-        console.log("Surgio un error en la peticion" +err);
-        return false; 
-    });
-    }catch(err){
-        console.log("Surgio un error");
-        return false;
+        })
+        .catch((err)=>{
+            console.log("Surgió un error en la petición" + err);
+        })
+    } catch (error) {
+        console.log("Surgió un error");
     }
-    return true;
 }
 
 function dibujarTabla(data){
-    //limpiar la tabla
-    document.getElementById("ventas").innerHTML=``;
-    //sacar el tipo de combox
     let tipo = document.getElementById("tipo").value;
-
-    for( let item of data){
-        if( item.tipo == tipo){
-            document.getElementById("ventas").innerHTML+=`
-            <tr>
-                <td>${item.folio}</td>
-                <td>${item.tipo}</td>
-                <td>${item.precio}</td>
-                <td>${item.descuento}</td>
-                <td>${item.total}</td>
-                <td>${item.fechapago}</td>
-                <td>${item.giro}</td>
-            </tr>`
+    document.getElementById("ventas").innerHTML=``
+    for(let item of data){
+        if(item.tipo ==tipo){
+            document.getElementById("ventas").innerHTML +=`
+                <tr>
+                    <th>${item.folio}</th>
+                    <th>${item.tipo}</th>
+                    <th>${item.precio}</th>
+                    <th>${item.descuento}</th>
+                    <th>${item.total}</th>
+                    <th>${item.fechapago}</th>
+                    <th>${item.giro}</th>
+                </tr>
+            `
         }
     }
 }
 
-const inicia = async()=>{
-    if(await cargarTipos() == true){
-        cargarTablaVentas();
-    }else{
-        console.log("Surgio un error");
-        
-    }
-    document.getElementById("tipo").addEventListener("change",async()=>{
-    cargarTablaVentas();
-});
-}
-    
 
+const inicia = async()=>{
+    if(await cargarTipos()==true){
+        cargarVentas();
+    }
+    document.getElementById("tipo").addEventListener("change", async()=>{
+        cargarVentas();
+    })
+}
 inicia();
